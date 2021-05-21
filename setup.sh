033e09bdd7153e2441d43dd3932bffb93e548b0d
giacomoquinalia <giacomo.quinaliaq@gmail.com>
2021-05-21 10:38:26 -0400

    Added Python Venv

--- setup.sh
+++ setup.sh
@@ -57,6 +57,9 @@ sudo npm install -g yarn
 # C lang
 sudo apt -y install gcc
 
+# Python
+sudo apt-get -y install python3-venv
+
 # Insomnia
 echo "deb [trusted=yes arch=amd64] https://download.konghq.com/insomnia-ubuntu/ default all" \
     | sudo tee -a /etc/apt/sources.list.d/insomnia.list
